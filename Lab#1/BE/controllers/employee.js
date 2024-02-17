acdfd2910bd89a5befee25c9b9cb2ef0b7640b69
Ahmed Yasser Hussein <93765400+AhmedYasser20@users.noreply.github.com>
2024-02-17 22:11:29 +0200

    Finished Lab1
    Write all TODOS in frontend and backend files

--- Lab#1/BE/controllers/employee.js
+++ Lab#1/BE/controllers/employee.js
@@ -1,5 +1,5 @@
 const employee = [
-  { id: '1', name: 'Mohamed Sayed' },
+  { id: '1', name: 'Mohamed Sayedsss' },
 ];
 
 exports.getEmployees = async (req, res, next) => {
@@ -8,8 +8,45 @@ exports.getEmployees = async (req, res, next) => {
 
 // TODO
 exports.deleteEmployee = async (req, res, next) => {
+  //Get ID form URL
+  const id=req.params.id;
+
+  //search in employee
+  let index = -1;
+  for (let i = 0; i < employee.length; i++) {
+    if(employee[i].id==id){
+      index=i;
+      break;
+    }
+  }
+  //const index = employee.findIndex((emp) => emp.id === id);
+  //check if we found the employee
+  if(index!=-1){
+    //The splice() method in JavaScript allows you to insert new elements into an array while deleting existing elements simultaneously.
+    employee.splice(index,1);
+    res.status(200).json({ message: 'Employee deleted successfully' });
+  }
+  else{
+    res.status(404).json({ message: 'Error' });
+  }
 };
 
 // TODO
 exports.createEmployee = async (req, res, next) => {
+   
+    const { id,name  } = req.body;
+    // Check if employee with the same id already exists
+    let index = -1;
+    for (let i = 0; i < employee.length; i++) {
+      if(employee[i].id==id){
+        index=i;
+        break;
+      }
+    }
+  //check if we found the employee
+  if(index!=-1){
+    return res.status(400).json({ message: 'Employee with the same id already exists' });
+  }
+    employee.push({ id, name: name });
+    res.status(201).json({ message: 'Employee created successfully' });
 };
--- Lab#1/FE/index.js
+++ Lab#1/FE/index.js
@@ -18,7 +18,9 @@ function fetchEmployees() {
         const deleteCell = document.createElement('td')
         const deleteButton = document.createElement('button');
         deleteButton.textContent = 'Delete';
+
         deleteButton.classList.add('btn', 'btn-danger', 'btn-sm');
+        deleteButton.addEventListener('click',()=> deleteEmployee(item.id)); // question
         deleteCell.appendChild(deleteButton);
 
         row.appendChild(deleteCell)
@@ -27,26 +29,68 @@ function fetchEmployees() {
       })
     })
     .catch(error => console.error(error))
+    //addEventListenerToDeleteButton();
 }
 
 // TODO
 // add event listener to submit button
 
+
+//The first parameter is the type of the event 
+//The second parameter is the function we want to call when the event occurs.
+
+const formElement=document.getElementById('employeeForm');
+const submitButton=formElement.getElementsByTagName("button");
+submitButton[0].addEventListener("click", createEmployee);
+//Another Solution
+//document.querySelector('button[type = "submit"]').addEventListener("click", createEmployee);
+//i think it is not perfect solution becouse what will happen if we have more than one buttom
+//and querySelector Get first match
+
 // TODO
 // add event listener to delete button
-
+//Question Here
+// function addEventListenerToDeleteButton(){
+//   const tableBody = document.getElementById('dataTable');
+//   const deletedCells=tableBody.getElementsByTagName("td");
+//     for (let i = 0; i < deletedCells.length; i++) {
+//       var id=deletedCells[i].textContent; 
+//       deletedCells[i].getElementsByTagName('button')[0].addEventListener("click", ()=>  deleteEmployee());
+//     }
+// }
 // TODO
 function createEmployee (){
   // get data from input field
+    const inputName=document.getElementById('name').value;
+    const inputid=document.getElementById('id').value;
   // send data to BE
+  if (!inputName || !inputid) return ;
+    try {
+      fetch('http://localhost:3000/api/v1/employee',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({id :inputid ,name: inputName})
+      });
+    } catch (error) {
+      console.error(error);
+    }
   // call fetchEmployees
+  fetchEmployees();
 }
 
 // TODO
-function deleteEmployee (){
+function deleteEmployee (id){
   // get id
+  // done
   // send id to BE
-  // call fetchEmployees
+  fetch(`http://localhost:3000/api/v1/employee/${id}`, {
+    method: 'DELETE',
+  }).then(response => response.json())
+  .then(() => fetchEmployees())
+  .catch(error => console.error('Error:', error));
+  // call fetchEmployeesr
 }
 
 fetchEmployees()
